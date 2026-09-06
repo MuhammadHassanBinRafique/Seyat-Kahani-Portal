caca030b5042c3df1c9b98f45bc4c9e445772176
MuhammahHassanBinRafique <bc230410283mhb@vu.edu.pk>
2026-09-06 13:19:51 +0500

    added live frontend link in app.js

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api/auth';
+const API_BASE_URL = 'https://seyat-kahani-portal-production.up.railway.app/api/auth';
 
 function switchTab(role) {
     const patientTab = document.querySelector('#patientTab');
